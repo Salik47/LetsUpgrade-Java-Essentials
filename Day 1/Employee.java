7da0b08b468e2e2f032397a7405588a3c9f95b68
Mohammed Salik Mulla <51821965+Salik47@users.noreply.github.com>
2020-11-01 23:53:13 +0530

    Create Employee.java

---
+++ Day 1/Employee.java
@@ -0,0 +1,14 @@
+public static String name = "Saurab";
+public static int age = 24;
+public static String city = "Chennai";
+
+public static void displayDetails() {
+    System.out.println("My name is " + name);
+    System.out.println("My age is " + age);
+    System.out.println("My city is" + city); 
+  }
+
+public static void main(String[] args) {
+    displayDetails();
+  }
+}
